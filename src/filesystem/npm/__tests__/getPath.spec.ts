e3a46261b5c9515c11cf3f95199e77579101a28b
Drake Costa <drake@saeris.io>
2022-08-13 12:18:39 -0700

    fix: Adjust matcher for `getPath` test

--- src/filesystem/npm/__tests__/getPath.spec.ts
+++ src/filesystem/npm/__tests__/getPath.spec.ts
@@ -2,8 +2,8 @@ import { getPath } from "../getPath";
 
 describe(`getPath`, () => {
   it(`resolves the path to a module`, async () => {
-    const actual = await getPath(`rollup`); //?
-    const expected = `suru\\node_modules\\rollup\\dist\\rollup.js`;
-    expect(actual.endsWith(expected)).toBeTruthy();
+    const actual = await getPath(`rollup`);
+    const expected = `suru\\node_modules\\rollup\\`;
+    expect(actual).toContain(expected);
   });
 });
