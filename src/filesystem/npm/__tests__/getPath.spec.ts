2454a6859df01e8a36ae0cd62bfcffe79037cc74
Drake Costa <drake@saeris.io>
2022-08-13 12:37:40 -0700

    fix: Use regex matcher for `getPath` test
    Looks like the test is failing because of a difference between forward and backslashes between Windows and Unix-like environments.

--- src/filesystem/npm/__tests__/getPath.spec.ts
+++ src/filesystem/npm/__tests__/getPath.spec.ts
@@ -3,7 +3,7 @@ import { getPath } from "../getPath";
 describe(`getPath`, () => {
   it(`resolves the path to a module`, async () => {
     const actual = await getPath(`rollup`);
-    const expected = `suru\\node_modules\\rollup\\`;
-    expect(actual).toContain(expected);
+    const expected = /suru[\\/]node_modules[\\/]rollup[\\/]/g; //?
+    expect(actual).toMatch(expected);
   });
 });
